940c5d69b39e18f8a501842e5e455a62f784e8a7
Igor Shchelchkov <podmihka@gmail.com>
2023-10-01 03:41:42 +0800

    fix get invite code repo

--- app/models/invite_code.go
+++ app/models/invite_code.go
@@ -5,8 +5,8 @@ import "time"
 type InviteCode struct {
 	ID             uint64
 	Code           string
-	OrganizationID uint64
-	OwnerID        uint64
+	OrganizationID uint64    `db:"organization_id"`
+	OwnerID        uint64    `db:"owner_id"`
 	CreatedAt      time.Time `db:"created_at"`
 	UpdatedAt      time.Time `db:"updated_at"`
 }
